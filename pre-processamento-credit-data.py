d4bc0e176b1f2e9c45cd4ee0d0489896328fea92
Wellison Maia <wellison_xadrez@hotmail.com>
2021-01-18 10:48:08 -0300

    Treatment of inconsistent values

--- pre-processamento-credit-data.py
+++ pre-processamento-credit-data.py
@@ -6,6 +6,18 @@ Created on Wed Jan 13 20:45:09 2021
 """
 import pandas as pd
 base = pd.read_csv('credit_data.csv')
+
 base.describe()
 
 base.loc[base['age'] < 0]
+# apagar a coluna
+base.drop('age', 1, inplace=True)
+# apagar somente os registros inconsistentes
+base.drop(base[base.age < 0].index, inplace=True)
+# preencher os valores manualmente (em caso de uma base pequena)
+
+# preencher os valores com a média (ideal)
+base.mean() #vejo a média de todos as colunas do meu dataframe
+base['age'].mean() #essa média ainda não corresponde a verdade, pois tem as idades inconsistentes
+base['age'][base.age > 0].mean() #encontro a média retirando os negativos
+base.loc[base.age < 0, 'age'] = 40.92 #insiro a média correta nas idades negativas
